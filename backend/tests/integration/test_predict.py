f0b6e9c377ca1de28dbd5af9151e22956594c2d8
Djamel Nafisse <djamel.nafisse@efrei.net>
2026-07-04 17:21:09 +0200

    test backend

---
+++ backend/tests/integration/test_predict.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_predict_endpoint():
+    payload = {
+        "median_income": 5,
+        "house_age": 20,
+        "avg_rooms": 5,
+        "avg_bedrooms": 1,
+        "population": 1000,
+        "avg_occupancy": 3,
+        "latitude": 34.05,
+        "longitude": -118.25,
+    }
+    response = client.post("/predict", json=payload)
+    assert response.status_code == 200
+    assert "predicted_price" in response.json()
